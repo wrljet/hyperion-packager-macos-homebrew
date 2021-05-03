f2f1f6beb5c13aa1f382db97b980e3842a56770a
Bill Lewis <wrljet@gmail.com>
2021-05-03 14:07:59 -0400

    Update hyperion-mojave.rb

--- hyperion-mojave.rb
+++ hyperion-mojave.rb
@@ -5,7 +5,7 @@ class HyperionMojave < Formula
   desc "SDL-Hercules-390 built for macOS"
   homepage ""
   url "https://github.com/wrljet/hyperion-mojave/archive/v0.9.1.tar.gz"
-  sha256 "bcb5fdc1819663ae84b7ef06723703d04e467ba17d8f11a19e6fcbc1a38d0561"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license ""
 
   def install
